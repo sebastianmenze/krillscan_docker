77228f52ce6f1c16831c5133cbf722d7feb329ad
Sebastian Menze <sebastian.menze@gmail.com>
2022-10-03 14:10:11 +0200

    Add files via upload

---
+++ echopy/__init__.py
@@ -0,0 +1,19 @@
+name = "echopy"
+__version__ = "0.0.2"
+
+from echopy import read_calibration
+from echopy import cmaps
+from echopy import correct_absorption
+from echopy import correct_soundspeed
+from echopy import get_background
+from echopy import mask_attenuated
+from echopy import mask_impulse
+from echopy import mask_multifrequency
+from echopy import mask_range
+from echopy import mask_seabed
+from echopy import mask_shoals
+from echopy import mask_signal2noise
+from echopy import mask_stationary
+from echopy import mask_transient
+from echopy import resample
+from echopy import transform
